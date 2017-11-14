718c6d9db0dd0269b39c853862c4877b447e1587
hsingfu <hsingfu.t@gmail.com>
2017-11-14 22:09:35 +1100

    added association

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,9 @@
 class User < ApplicationRecord
+
+	has_secure_password # adds 2 methods to the user object: 
+	#.password is the first method added
+	#.authenticate is the second method added
+
+	has_many :offers
+
 end
