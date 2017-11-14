0d8eb2291aedf8c459f42a2b834c9e85dffc7d64
hsingfu <hsingfu.t@gmail.com>
2017-11-14 22:51:42 +1100

    added api route

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,12 @@ Rails.application.routes.draw do
   resources :users
   resources :offers
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get '/api/offers', to: 'api/offers#index'
+
+  # to create api folder routes
+  namespace :api do
+  	resources :comments
+  end
+
 end
